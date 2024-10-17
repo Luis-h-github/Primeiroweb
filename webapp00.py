9a78ec54cf91d850f31368ae97d42e842fc58502
Luis-h-github <Luizhenriquer0911@gmail.com>
2024-10-17 20:55:19 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -14,8 +14,6 @@ st.subheader("Primeiro passo")
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
 
-import pandas as pd
-
 df = pd.read_csv('data.csv')
 
 print(df.to_string()) 
