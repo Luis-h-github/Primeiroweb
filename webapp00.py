55815b44a9f68495510d4e937a70a303d1bffa01
Luis-h-github <Luizhenriquer0911@gmail.com>
2024-10-17 20:37:38 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -11,13 +11,13 @@ Configurar_Pagina("Exemplo 1",
                     "©️")
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
+st.title("Criando um aplicativo do zero")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("primeiro para para o desenvolvimento")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("vamos começar seguindo a seguinte instrução")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
